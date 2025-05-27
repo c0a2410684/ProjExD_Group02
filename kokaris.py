4b29652b8041be30e5e4204ba2358db16c04af6d
arata <c0a24226ed@edu,teu.ac.jp>
2025-05-27 15:32:48 +0900

    ホールド

--- kokaris.py
+++ kokaris.py
@@ -10,27 +10,26 @@ MAX_COL = 10
 BLOCK_SIZE = 35
 GRID_OFFSET_X = 30
 GRID_OFFSET_Y = 30
-FALL_SPEED = 30  # 一定の落下速度 (フレーム数)
+FALL_SPEED = 30
 
 class Block:
     def __init__(self, block_type):
         self.shapes = [[], [],  # empty block and wall
-                       [[0, -1], [0, 0], [0, 1], [0, 2]],  # I block
-                       [[-1, -1], [0, -1], [0, 0], [0, 1]],  # J block
-                       [[0, -1], [0, 0], [0, 1], [-1, 1]],  # L block
-                       [[0, -1], [0, 0], [-1, 0], [-1, 1]],  # S blosk
-                       [[-1, -1], [-1, 0], [0, 0], [0, 1]],  # Z block
-                       [[0, -1], [0, 0], [-1, 0], [0, 1]],  # T block
-                       [[0, 0], [-1, 0], [0, 1], [-1, 1]]]  # square
+                       [[0, -1], [0, 0], [0, 1], [0, 2]],  # I
+                       [[-1, -1], [0, -1], [0, 0], [0, 1]],  # J
+                       [[0, -1], [0, 0], [0, 1], [-1, 1]],  # L
+                       [[0, -1], [0, 0], [-1, 0], [-1, 1]],  # S
+                       [[-1, -1], [-1, 0], [0, 0], [0, 1]],  # Z
+                       [[0, -1], [0, 0], [-1, 0], [0, 1]],  # T
+                       [[0, 0], [-1, 0], [0, 1], [-1, 1]]]  # O
 
         self.block_type = block_type
         self.shape = copy.deepcopy(self.shapes[block_type])
-        self.row = 1  # initial position
+        self.row = 1
         self.col = 5
         self.count = 0
 
-    # key command movement
-    def move(self, board, direction):  # direction down:0 left:1 right:2
+    def move(self, board, direction):
         if direction == 0 and self.moveable(board, [1, 0]):
             self.row += 1
         elif direction == 1 and self.moveable(board, [0, -1]):
@@ -40,17 +39,16 @@ class Block:
 
     def moveable(self, board, direction):
         drow, dcol = direction
-
         for dx in self.shape:
             row = self.row + dx[0] + drow
             col = self.col + dx[1] + dcol
             if not (0 <= row < MAX_ROW + 3 and 0 <= col < MAX_COL + 2 and board[row][col] == 0):
                 return False
-
         return True
 
-    def rotate(self, board, direction):  # clockwise:0 anticloskwise:1
-        # long bar rotates differently
+    def rotate(self, board, direction):
+        old_shape = copy.deepcopy(self.shape)
+
         if self.block_type == 2:
             if direction == 0:
                 for dx in self.shape:
@@ -58,36 +56,28 @@ class Block:
             elif direction == 1:
                 for dx in self.shape:
                     dx[0], dx[1] = 1 - dx[1], dx[0]
-
-        # square doesn`t rotate
         elif self.block_type == 8:
-            pass
+            return
+        else:
+            if direction == 0:
+                for dx in self.shape:
+                    dx[0], dx[1] = dx[1], -dx[0]
+            elif direction == 1:
+                for dx in self.shape:
+                    dx[0], dx[1] = -dx[1], dx[0]
 
-        # other blocks
-        elif direction == 0:
-            for dx in self.shape:
-                dx[0], dx[1] = dx[1], -dx[0]
-        elif direction == 1:
-            for dx in self.shape:
-                dx[0], dx[1] = -dx[1], dx[0]
-
-        self.rotate_correction(board)
+        if not self.rotate_correction(board):
+            self.shape = old_shape
 
     def rotate_correction(self, board):
-        move_priority = [[0, 0], [0, -1], [0, 1], [-1, 0], [1, 0], [2, 0], [-1, 1], [1, 1]]
+        move_priority = [[0, 0], [0, -1], [0, 1], [-1, 0], [1, 0]]
         for direction in move_priority:
             if self.moveable(board, direction):
                 self.row += direction[0]
                 self.col += direction[1]
-                return
+                return True
+        return False
 
-        direction = [0, 2]
-        while not self.moveable(board, direction):
-            direction[1] += 1
-        self.row += direction[0]
-        self.col += direction[1]
-
-    # moving downward due to time
     def drop(self, board):
         if self.count < FALL_SPEED:
             self.count += 1
@@ -97,7 +87,7 @@ class Block:
             self.row += 1
             return 0
         else:
-            return 1  # make new block
+            return 1
 
     def draw(self, screen, block_color):
         for row, col in self.shape:
@@ -112,12 +102,11 @@ class Block:
         for dx in self.shape:
             row = self.row + dx[0]
             col = self.col + dx[1]
-            if not (2 <= row < MAX_ROW + 2 and 1 <= col < MAX_COL + 1):  # placed block outside screen
+            if not (2 <= row < MAX_ROW + 2 and 1 <= col < MAX_COL + 1):
                 return 1
             board[row][col] = self.block_type
         return 0
 
-# ブロックとボードの初期化
 def initialize_game():
     board = [[0 for _ in range(MAX_COL + 2)] for _ in range(MAX_ROW + 3)]
     for col in range(MAX_COL + 2):
@@ -128,11 +117,8 @@ def initialize_game():
 
     block_type = random.randint(2, 8)
     block = Block(block_type)
-
     return board, block
 
-# 入力　ボード
-# 出力　消える行数、消える行の番号
 def find_deleting_row(board):
     count = 0
     row_numbers = []
@@ -142,49 +128,28 @@ def find_deleting_row(board):
             row_numbers.append(row)
     return count, row_numbers
 
-# 行削除
-# 入力　スクリーン、ボード、消す行番号
-# 出力　なし
 def delete_row(screen, board, row_number, block_color):
-    n_col = 4
     for row in row_number:
         for col in range(1, MAX_COL + 1):
             board[row][col] = 0
-    for _ in range(n_col + MAX_COL):
-        for row in row_number:
-            for col in reversed(range(1, MAX_COL + 1)):
-                board[row][col] = board[row][col - 1]
-            if _ < n_col:
-                board[row][1] = 9
-        pygame.time.wait(8)
-        draw_board(screen, board, block_color)
-        pygame.display.update()
-
     for deleting_row in row_number:
         for row in reversed(range(2, deleting_row + 1)):
             for col in range(1, MAX_COL + 1):
                 board[row][col] = board[row - 1][col]
 
-# ゲームボードの描画
-# 入力　スクリーン、ゲームボード、ブロックの色
-# 出力　なし
 def draw_board(screen, board, block_color):
     for row in range(2, MAX_ROW + 3):
         for col in range(MAX_COL + 2):
             draw_x = GRID_OFFSET_X + BLOCK_SIZE * col
             draw_y = GRID_OFFSET_Y + BLOCK_SIZE * (row - 2)
             pygame.draw.rect(screen, (0, 0, 0), Rect(draw_x, draw_y, BLOCK_SIZE, BLOCK_SIZE))
-            if board[row][col] < 2:
-                pygame.draw.rect(screen, block_color[board[row][col]],
-                                 Rect(draw_x + 1, draw_y + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2))
-            else:
-                pygame.draw.rect(screen, block_color[board[row][col]],
-                                 Rect(draw_x + 2, draw_y + 2, BLOCK_SIZE - 4, BLOCK_SIZE - 4))
+            pygame.draw.rect(screen, block_color[board[row][col]],
+                             Rect(draw_x + 1, draw_y + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2))
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1000, 770))
-    pygame.display.set_caption("KOKARIS")  # title bar
+    pygame.display.set_caption("KOKARIS")
 
     block_color = [(50, 50, 50), (150, 150, 150), (255, 0, 0), (0, 0, 255), (255, 165, 0),
                    (255, 0, 255), (0, 255, 0), (0, 255, 255), (255, 255, 0), (200, 200, 200), (100, 100, 100)]
@@ -193,16 +158,16 @@ def main():
     next_block_type = random.randint(2, 8)
 
     game_over = False
+    hold_block = None
+    can_hold = True
 
     while not game_over:
         pygame.time.wait(10)
-
-        screen.fill((0, 0, 0))  # fill with black
+        screen.fill((0, 0, 0))
 
         draw_board(screen, board, block_color)
         block.draw(screen, block_color)
 
-        # move command
         pressed_key = pygame.key.get_pressed()
         if pressed_key[K_DOWN]:
             block.move(board, 0)
@@ -219,41 +184,54 @@ def main():
                 count, row_numbers = find_deleting_row(board)
                 if count > 0:
                     delete_row(screen, board, row_numbers, block_color)
-
                 block = Block(next_block_type)
                 next_block_type = random.randint(2, 8)
+                can_hold = True
                 if not block.moveable(board, [0, 0]):
                     game_over = True
 
-        pygame.display.update()
+        if hold_block is not None:
+            for dx in hold_block.shape:
+                row = 2 + dx[0]
+                col = 15 + dx[1]
+                pygame.draw.rect(screen, block_color[hold_block.block_type],
+                                 Rect(GRID_OFFSET_X + col * BLOCK_SIZE,
+                                      GRID_OFFSET_Y + row * BLOCK_SIZE, BLOCK_SIZE - 4, BLOCK_SIZE - 4))
 
+        pygame.display.update()
         for event in pygame.event.get():
-            # close button
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
             if event.type == KEYDOWN:
-                # escape key pressed
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-
-                # block rotation
-                if event.key == K_a or event.key == K_SPACE:  # anti-clockwise
+                if event.key == K_LSHIFT:
+                    if can_hold:
+                        if hold_block is None:
+                            hold_block = block
+                            block = Block(next_block_type)
+                            next_block_type = random.randint(2, 8)
+                        else:
+                            hold_block, block = block, hold_block
+                        block.row, block.col = 1, 5
+                        can_hold = False
+                elif event.key == K_a or event.key == K_SPACE:
                     block.rotate(board, 1)
-                if event.key == K_s:  # clockwise
+                elif event.key == K_s:
                     block.rotate(board, 0)
 
-    # Game Over: Do nothing, the loop will just stop
     while game_over:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
+            for event in pygame.event.get():
+                if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
 
 if __name__ == "__main__":
     main()
